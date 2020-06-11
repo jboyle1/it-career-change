411091cb90b93e224e558e7bcba750cf37aace8b
Jed Boyle <jedboyle01@gmail.com>
2020-06-11 13:45:07 +0100

    (Games of Chance) 001 - Import the random module and create a variable called money that starts with 100.

--- games-of-chance/gamesofchance/gamesofchanceapp/models.py
+++ games-of-chance/gamesofchance/gamesofchanceapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 001 - Import the random module and create a variable called money that starts with 100.
+import random
+
+money = 100
+
+
+
