cda3700a89e1bce71d07dfc29cdac52680b470a5
Jed Boyle <jedboyle01@gmail.com>
2020-06-11 14:06:29 +0100

    Created random coin flip and first conditional statment signifying a winning 'Heads' coin flip.

--- games-of-chance/gamesofchance/gamesofchanceapp/models.py
+++ games-of-chance/gamesofchance/gamesofchanceapp/models.py
@@ -8,5 +8,25 @@ import random
 
 money = 100
 
+# 002 - Create a function that simulates flipping a coin and calling either "Heads" or "Tails".
+def coin_flip(guess, bet):
+    # 'coinside' generates a side of the coin that wins.
+    coinSide = random.randint(1,2)
+    
+    # First conditional statement allows for a winning argument of 'Heads'. 1 is a match to 'Heads.
+    if guess == "Heads" and coinSide == 1:
+        # 'totalMoney equals the starting money plus the bet.
+        totalMoney = money + bet
+        # print out the bet as an integer.
+        print(int(bet))
+        # print out string iterpolated with totalMoney
+        headsWins = "Heads, you won! You now have {}"
+        headsWins.format(totalMoney)
+        print(headsWins)
+
+    
+
+
+
 
 
