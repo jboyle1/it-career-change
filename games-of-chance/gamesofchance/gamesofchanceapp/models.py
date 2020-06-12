754b2a051918f64a0e11d14709f1e9ac0dbce930
Jed Boyle <jedboyle01@gmail.com>
2020-06-12 11:42:59 +0100

    Update 'coin_flip()' function so you can choose to play again.

--- games-of-chance/gamesofchance/gamesofchanceapp/models.py
+++ games-of-chance/gamesofchance/gamesofchanceapp/models.py
@@ -7,29 +7,45 @@ from django.db import models
 # 001 - Import the random module and create a variable called money that starts with 100.
 import random
 
-money = 100
+totalMoney = 100
 
-# Get values for wager and guess variables
-name = input("Please Enter a Username: ")
-print("\n")
-print("Hello {}".format(name))
-print("Lets play a game of Heads or tails!")
-print("\n")
-bet1 = input("Please enter your wager: ")
-wager1 = int(bet1)
-guess = input("Please enter your guess (Heads or Tails): ")
+def greeting():
+    global name
+    name = input("Please Enter a Username: ")
+    print("\n")
+    print("Hello {}".format(name))
+    print("Lets play a game of Heads or tails!")
+    print("\n")
+    return name
+
+# Get value for wager1 and guess variables
+
+def wager1_value():
+    global wager1
+    print("You have {}".format(totalMoney))
+    print("\n")
+    bet1 = input("Please enter your wager: ")
+    wager1 = int(bet1)
+    return wager1
+
+def guess_value():
+    global guess
+    print("\n")
+    guess = input("Please enter your guess (Heads or Tails): ")
+    return guess
 
 # 002 - Create a function that simulates flipping a coin and calling either "Heads" or "Tails".
 def coin_flip(guess, wager1):
     # 'coinside' generates a side of the coin that wins.
     coinSide = random.randint(1,2)
-
+    global totalMoney
     # First conditional statement allows for a winning argument of 'Heads'. 1 is a match to 'Heads.
     if guess == "Heads" and coinSide == 1:
         # 'totalMoney' equals the starting money plus the bet.
-        totalMoney = money + (wager1 * 2)
+        totalMoney = totalMoney + (wager1 * 2)
         # print out the bet as an integer.
         yourBet = int(wager1)
+        print("\n")
         yourBetString = "Your bet is {}".format(yourBet)
         print(yourBetString)
         # print out string iterpolated with totalMoney
@@ -40,9 +56,10 @@ def coin_flip(guess, wager1):
     # elif statement allows for a winning argument of 'Tails'. 2 is a match to 'Tails.
     elif guess == "Tails" and coinSide == 2:
         # 'totalMoney' equals the starting money plus the bet.
-        totalMoney = money + wager1
+        totalMoney = totalMoney + wager1
         # print out the bet as an integer.
         yourBet = int(wager1)
+        print("\n")
         yourBetString = "Your bet is {}".format(yourBet)
         print(yourBetString)
         # print out string iterpolated with totalMoney
@@ -53,9 +70,10 @@ def coin_flip(guess, wager1):
     # else statement prints if you loose. E.g if logical comparison operators do not match.
     else:
         # 'totalMoney' equals the starting money subtracted by the bet as the user lost.
-        totalMoney = money - wager1
+        totalMoney = totalMoney - wager1
         # print out the bet as an integer (subtracted).
         yourBet = int(wager1)
+        print("\n")
         yourBetString = "Your bet is {}".format(yourBet)
         print(yourBetString)
         # print out string iterpolated with totalMoney
@@ -63,24 +81,51 @@ def coin_flip(guess, wager1):
         print(lost)
         return totalMoney
 
+def play_coin_flip_again():
+    playCoinFlipAgain = input("Would you like to play again! Y/N: ")
+    if playCoinFlipAgain == "Y":
+        wager1_value()
+        guess_value()
+        coin_flip(guess, wager1)
+        play_coin_flip_again()
+    else:
+        print("\n")
+        print("{}, lets play a game of Cho-Han!".format(name))
+        print("\n")
+
 # Call coin_flip function
+greeting()
+wager1_value()
+guess_value()
 coin_flip(guess, wager1)
+play_coin_flip_again()
 
 
 # --- Cho-Han Function ---
 
-# Get values for wager and guess variables
-print("\n")
-print("{}, lets play a game of Cho-Han!".format(name))
-print("\n")
-bet2 = input("Please enter your wager: ")
-wager2 = int(bet2)
-oddOrEven = input("Please enter odd or even: ")
 
 
 # 003 - Create a function that simulates rolling two dice and adding the results together. The player predicts whether the sum of those dice is odd or even and wins if their prediction is correct.
+# Get value for wager1 and guess variables
 
-def cho_han(oddOrEven, wager2, name):
+def wager2_value():
+    global wager2
+    print("You have {}".format(totalMoney))
+    bet2 = input("Please enter your wager: ")
+    wager2 = int(bet2)
+    return wager2
+
+def odd_or_even_value():
+    global oddOrEven
+    oddOrEven = input("Please enter odd or even: ")
+    return oddOrEven
+
+def cho_han(oddOrEven, wager2, name, totalMoney):
+    # Get values for wager and guess variables
+    bet2 = input("Please enter your wager: ")
+    wager2 = int(bet2)
+    oddOrEven = input("Please enter odd or even: ")
+
     # 'dice1' & 'dice2' each generates a number between 1 -6.
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
@@ -99,23 +144,39 @@ def cho_han(oddOrEven, wager2, name):
     # Write a conditional statement that uses logical operators to find out if the left over modulo value in 'sumOfTwoDice' is odd or even and prints if the user has guessed correctly. This in turn adds or subtracts the totalMoney value. 
     if sumOfTwoDice == 0 and oddOrEven == "even":
         # Add the 'totalMoney' to the winnings
-        totalMoney = money + (wager2 * 2)
+        totalMoney = totalMoney + (wager2 * 2)
         print("Even!")
         print("You have won {}! you now have {}".format(name, totalMoney))
         return totalMoney
     # Create an elif statement that prints if the 'sumOfTwoDice' is odd.
     elif sumOfTwoDice != 0 and oddOrEven == "odd":
          # Add the 'totalMoney' to the winnings
-        totalMoney = money + (wager2 * 2)
+        totalMoney = totalMoney + (wager2 * 2)
         print("Odd!")
         print("You have won {}! you now have {}".format(name, totalMoney))
         return totalMoney
     else:
-        totalMoney = money - wager2
+        totalMoney = totalMoney - wager2
         print("Sorry {}, you lost! you now have {}".format(name, totalMoney))
         return totalMoney
 
-cho_han(oddOrEven, wager2, name)
+wager2_value()
+odd_or_even_value()
+cho_han(oddOrEven, wager2, name, totalMoney)
 
 
+# --- Cho-Han Function ---
 
+# Get values for wager and guess variables
+
+print("\n")
+print("{}, lets play a game of highest card wins".format(name))
+print("\n")
+bet3 = input("Please enter your wager: ")
+wager3 = int(bet2)
+userCard = input("Please pick a card! Please type in the following format - king: diamonds")
+computerCard = random.randint(0, 53)
+
+# 004 - Create a function that simulates two players picking a card randomly from a deck of cards. The higher number wins.
+
+# def highest_card(wager3, name, userCard, computerCard):
