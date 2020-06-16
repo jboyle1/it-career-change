e40fdafe83f0ce7ceba5b7bd135f1a4d2dd209f0
Jed Boyle <jedboyle01@gmail.com>
2020-06-16 11:31:45 +0100

    Add index.html and source code from Codecademy to check 'generateTarget()' function in dev tools console.

--- number-guesser/js/script.js
+++ number-guesser/js/script.js
@@ -4,11 +4,20 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-// 001 Gernerate a number from 1 to 10.
+// 001 Gernerate a number from 1 to 10. This is the secret target number.
 
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10)
+    return Math.floor(Math.random() * 10);
 }
 
+console.log(generateTarget());
 
+const compareGuesses = (userGuess, computerGuess, targetGuess) => {
+
+    const userDifference = getAbsoluteDistance(userGuess, targetGuess);
+    const computerDifference = getAbsoluteDistance(computerGuess, targetGuess);
+    alertMessage(userGuess);
+
+    return computerDifference >= userDifference;
+}
 
