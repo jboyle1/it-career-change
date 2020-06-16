de224490ef034645e7d6ef47cfd0bccd69694e14
Jed Boyle <jedboyle01@gmail.com>
2020-06-16 11:49:16 +0100

    Completed number Guesser game.

--- number-guesser/js/script.js
+++ number-guesser/js/script.js
@@ -2,38 +2,40 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Write your code below: 001 Gernerate a number from 1 to 10. This is the
+// secret target number.
 
-// 001 Gernerate a number from 1 to 10. This is the secret target number.
-
-const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
+function generateTarget() {
+    return Math.floor(Math.random() * Math.floor(9));
 }
 
-
-// console.log(generateTarget());
-
-// 002 create a function called 'compareGuesses()' to determine which player wins based on which guess is closest to the target. If both players are tied, the human user should win. Return true if the human player wins, and false if the computer player wins.
-const compareGuesses = (userGuess, computerGuess, targetGuess) => {
-
-    const userDifference = getAbsoluteDistance(userGuess, targetGuess);
-    const computerDifference = getAbsoluteDistance(computerGuess, targetGuess);
-    alertMessage(userGuess);
-
-    return computerDifference >= userDifference;
+// 002 create a function called 'compareGuesses()' to determine which player
+// wins based on which guess is closest to the target. If both players are tied,
+// the human user should win. Return true if the human player wins, and false if
+// the computer player wins.
+function compareGuesses(humanGuessInput, target, computerGuess) {
+    if (Math.abs(humanGuessInput - target) <= Math.abs(computerGuess - target)) {
+        return true;
+    }
+    if (Math.abs(humanGuessInput - target) >= Math.abs(computerGuess - target)) {
+        return false;
+    }
 }
 
-// 003 This function will be used to correctly increase the winner’s score after each round.
+// 003 This function will be used to correctly increase the winner’s score after
+// each round.
 
-const updateScore = winner =>{
+function updateScore(winner) {
+    if (winner == 'human') {
+        return humanScore++
+    }
+    if (winner == "computer") {
+        return computerScore++
+    } else {}
+}
 
-	if (winner === 'human'){
+// This function will be used to update the round number after each round.
 
-		humanScore++;
-
-	}else if(winner === 'computer'){
-
-		computerScore++;
-	}
-
+function advanceRound() {
+    return currentRoundNumber++
 }
