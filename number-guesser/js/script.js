72e3c61fbab33e236e34c4bb8897754328589a88
Jed Boyle <jedboyle01@gmail.com>
2020-06-16 11:18:30 +0100

    Add first function for number guesser. generateTarget().

---
+++ number-guesser/js/script.js
@@ -0,0 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+// 001 Gernerate a number from 1 to 10.
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
+
+
+
